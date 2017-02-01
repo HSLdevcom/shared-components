b0e13f6a3584bd06999da53d7c9e983d60ba9853
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-02-01 12:55:19 +0200

    added AppHeader component as a test

---
+++ AppHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppHeader = styled.div `
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+export default AppHeader;
