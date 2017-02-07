4833bb0806362f3ccdb678fae6755f2c56eb7707
Jaakko Hämäläinen <jaakko.hamalainen@tieto.com>
2017-02-07 12:15:07 +0200

    css file added to testServer. This adds the gotham font-family, which is used in hsl. Some styling changes in input component

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -2,13 +2,15 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Input = styled.input `
-font-size: 1.25em;
-padding: 0.5em;
-margin: 0.5em;
-color: #222222;
+font-size: 0.75em;
+font-weight: bold;
+padding: 1.25em;
+margin: 1.25em;
+color: #737373;
 background: #FFFFFF;
-border: 1px solid #dbdbdb;
+border: 0.15em solid #dbdbdb;
 border-radius: 5px;
+text-transform: uppercase;
 font-family: "Gotham Rounded SSm A","Gotham Rounded SSm B", Arial, Georgia, Serif;
 
 &::-webkit-input-placeholder {
--- testServer/src/App.js
+++ testServer/src/App.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line
+import * as styles from './styles.css';
 import React, {Component} from 'react';
 import {ThemeProvider} from 'styled-components';
 import ComponentList from './ComponentList';
+
 
 const hslTheme = {
   primary: 'blue',
