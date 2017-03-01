e6895356fb6bdf3085f599217ba4062a775b6737
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-03-01 16:25:43 +0200

    container title has different typography

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -6,6 +6,13 @@ const Container = styled.div`
   background-color: #fff;
   width: 100%;
   padding: 16pt;
+  h1 {
+    font-size: 18pt;
+    letter-spacing: -0.4pt;
+    color: #333;
+    text-align: center;
+    margin: 0 auto 16pt auto;
+  }
 `;
 
 export default Container;
