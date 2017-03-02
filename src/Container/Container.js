c4a4fbebd3fce52e2694ac5e608796e7cc7d5a24
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-03-02 14:32:03 +0200

    chagend pagetitle and container pt amounts to px and fiddled with details

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  border-radius: 5pt;
+  border-radius: 10px;
   border: 1pt solid #dddddd;
   background-color: #fff;
   width: 100%;
-  padding: 16pt;
+  padding: 20px;
   h1 {
-    font-size: 18pt;
-    letter-spacing: -0.4pt;
+    font-size: 22px;
+    letter-spacing: -0.8px;
     color: #333;
     text-align: center;
-    margin: 0 auto 16pt auto;
+    margin: 0 auto 20px auto;
   }
 `;
 
--- src/PageTitle/PageTitle.js
+++ src/PageTitle/PageTitle.js
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 
 
 const PageTitle = styled.h1`
- font-size: 15pt;
- letter-spacing: -0.4pt;
- color: #333;
- text-align: center;
- margin: 19pt auto;
- font-weight: normal;
+  font-size: 20px;
+  letter-spacing: -0.8px;
+  color: #333;
+  text-align: center;
+  margin: 16px auto;
+  font-weight: normal;
 `;
 
 export default PageTitle;
