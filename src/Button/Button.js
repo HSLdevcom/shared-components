2ded6d537eda334f4379c047bd8993f22b2499e9
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-03-22 15:51:05 +0200

    changed button values to ems and secondary button to transparent, removed forced button height

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,13 +2,14 @@ import styled from 'styled-components';
 
 
 const Button = styled.button`
-  height: 40pt;
-  border-radius: 20pt;
+  height: auto;
+  border-radius: 2em;
   border-color: ${props => props.theme.primary};
   border-style: solid;
-  background-color: ${props => (props.primary ? props.theme.primary : props.theme.secondary)};
+  background-color: ${props => (props.primary ? props.theme.primary : 'transparent')};
   color: ${props => (props.primary ? props.theme.secondary : props.theme.primary)};
-  font-size: 15pt;
+  padding: 0.7em 1.6em;
+  font-size: 1.1em;
   letter-spacing: -0.4pt;
   width: 90%;
   display: block;
