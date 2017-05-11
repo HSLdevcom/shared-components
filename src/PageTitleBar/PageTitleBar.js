70b758dcfd76ee4b019ef2204e35d1d9cc3d057e
Meuronen Ilari <ilari.meuronen@tieto.fi>
2017-05-11 13:55:08 +0300

    pagetitlebar max-width to hsl limit 1100px

--- src/PageTitleBar/PageTitleBar.js
+++ src/PageTitleBar/PageTitleBar.js
@@ -3,41 +3,46 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Div = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
   position: relative;
   background-color: #fff;
   width: 100vw;
   left: calc(-50vw + 50%);
   margin-bottom: 1em;
-  h1 {
-    font-size: 1.2em;
-    letter-spacing: -0.8px;
-    color: ${props => props.theme.primary};
-    text-align: center;
-    margin: 1em auto;
-    font-weight: normal;
-  }
-  button {
+  .content-container {
+    position: relative;
     display: flex;
+    flex-direction: row;
     align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 0;
-    left: 0;
-    font-size: 1.2em;
-    width: 3em;
-    height: 100%;
-    text-align: center;
-    border-width: 0;
-    border: none;
-    background-color: #fff;
-    border-right: 1px solid #ddd;
-    padding: 0;
-    img {
-      height: 1em;
-      margin: auto;
+    max-width: 1100px;
+    margin: auto;
+    h1 {
+      font-size: 1.2em;
+      letter-spacing: -0.8px;
+      color: ${props => props.theme.primary};
+      text-align: center;
+      margin: 1em auto;
+      font-weight: normal;
+    }
+    button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      font-size: 1.2em;
+      width: 3em;
+      height: 100%;
+      text-align: center;
+      border-width: 0;
+      border: none;
+      background-color: #fff;
+      border-right: 1px solid #ddd;
+      padding: 0;
+      img {
+        height: 1em;
+        margin: auto;
+      }
     }
   }
 `;
@@ -45,12 +50,14 @@ const Div = styled.div`
 
 const PageTitleBar = ({ children, text, backButton, onBackClick, className }) =>
   <Div className={className}>
-    {backButton &&
-      <button onClick={onBackClick}>
-        <img src={backButton} alt="placeholder" />
-      </button>
-    }
-    {children}
+    <div className="content-container">
+      {backButton &&
+        <button onClick={onBackClick}>
+          <img src={backButton} alt="placeholder" />
+        </button>
+      }
+      {children}
+    </div>
   </Div>;
 
 PageTitleBar.propTypes = {
