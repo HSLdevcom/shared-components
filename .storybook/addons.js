b7e550b7a1c88dcd92d2dd0dcfcc9274628989d7
Julius Räihä <julius.raiha@phz.fi>
2017-05-22 14:48:10 +0300

    Storybook: actions & knobs addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,5 @@
 import '@kadira/storybook/addons'
+import '@kadira/storybook-addon-actions/register'
+import '@kadira/storybook-addon-knobs/register'
 import 'storybook-addon-jsx/register'
+
--- stories/index.js
+++ stories/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { setAddon, storiesOf } from '@kadira/storybook'
+import { action } from '@kadira/storybook-addon-actions'
 import JSXAddon from 'storybook-addon-jsx'
 import Button from '../src/Button/Button'
 import Input from '../src/Input/Input'
+import PageTitleBar from '../src/PageTitleBar/PageTitleBar'
+import arrow from './static/arrow_left.svg'
 
 setAddon(JSXAddon)
 
@@ -12,3 +15,13 @@ storiesOf('Button', module)
 
 storiesOf('Input', module)
   .addWithJSX('with placeholder', () => <Input placeholder="placeholder" type="text" />, { displayName: 'Input' })
+
+storiesOf('PageTitleBar', module)
+	.addWithJSX('with back button', () =>
+		<PageTitleBar backButton={arrow} onBackClick={action('back button clicked')}>
+      <h1>Title</h1>
+    </PageTitleBar>)
+	.addWithJSX('without back button', () =>
+		<PageTitleBar>
+      <h1>Title</h1>
+    </PageTitleBar>)
