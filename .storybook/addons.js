c778a0414f8b6f7024ef582b9c0c8e140ed3327e
Julius Räihä <julius.raiha@phz.fi>
2017-05-26 09:52:58 +0300

    Updated JSX addon so it works with knobs addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 import '@kadira/storybook/addons'
 import '@kadira/storybook-addon-actions/register'
+import '@kadira/storybook-addon-knobs/register'
 import 'storybook-addon-jsx/register'
 
--- stories/Nav.stories.js
+++ stories/Nav.stories.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router';
-import { setAddon, storiesOf } from '@kadira/storybook'
-import { action } from '@kadira/storybook-addon-actions'
-import JSXAddon from 'storybook-addon-jsx'
-import Nav from '../src/Nav/Nav'
-import logo from './static/logo.png'
+import { setAddon, storiesOf } from '@kadira/storybook';
+import { action } from '@kadira/storybook-addon-actions';
+import { withKnobs, text } from '@kadira/storybook-addon-knobs';
+import JSXAddon from 'storybook-addon-jsx';
+import Nav from '../src/Nav/Nav';
+import logo from './static/logo.png';
 
-setAddon(JSXAddon)
+setAddon(JSXAddon);
 
 const languages = [{ id: 'fi', name: 'FI' }, { id: 'sv', name: 'SV' }, { id: 'en', name: 'EN' }];
 
-storiesOf('Nav', module)
-  .addWithJSX('without title', () =>
-  	<Nav logo={logo} alt={'HSL'} languages={languages} changeLanguage={action('language changed')}>
+const stories = storiesOf('Nav', module);
+
+stories.addDecorator(withKnobs);
+
+stories.addWithJSX('without title', () =>
+  (<Nav logo={logo} alt={'HSL'} languages={languages} changeLanguage={action('language changed')}>
+    <Link to="/">Koti</Link>
+    <Link to="/test">Minun kortit</Link>
+    <Link to="/test">Asetukset</Link>
+    <Link to="/test">Kirjaudu ulos</Link>
+  </Nav>)
+)
+  .addWithJSX('with title', () => {
+    const title = text('Title', 'Some title');
+    return (<Nav logo={logo} alt={'HSL'} languages={languages} changeLanguage={action('language changed')} title={title}>
       <Link to="/">Koti</Link>
       <Link to="/test">Minun kortit</Link>
       <Link to="/test">Asetukset</Link>
       <Link to="/test">Kirjaudu ulos</Link>
-    </Nav>)
-  .addWithJSX('with title', () =>
-  	<Nav logo={logo} alt={'HSL'} languages={languages} changeLanguage={action('language changed')} title="Page title">
-      <Link to="/">Koti</Link>
-      <Link to="/test">Minun kortit</Link>
-      <Link to="/test">Asetukset</Link>
-      <Link to="/test">Kirjaudu ulos</Link>
-    </Nav>)
+    </Nav>);
+  });
