63e2fa0c861a353817b50220411de7d3789b9028
Julius Räihä <julius.raiha@phz.fi>
2017-06-19 15:30:42 +0300

    Reduced More & Latest icons size to 80%. Now both match other icons in size when they are next to each other

--- src/Icons/More.js
+++ src/Icons/More.js
@@ -2,7 +2,7 @@ import React from 'react';
 /* eslint-disable */
 export default function({ fill, height, width }) {
   return (
-    <svg fill={fill} height={height} width={width} viewBox="0 0 38 10" version="1.1" preserveAspectRatio="xMidYMid meet">
+    <svg fill={fill} height={height} width={width} viewBox="-5 0 48 10" version="1.1" preserveAspectRatio="xMidYMid meet">
       <g id="Group">
                 <circle id="Oval" cx="5" cy="5" r="5"></circle>
                 <circle id="Oval" cx="19" cy="5" r="5"></circle>
