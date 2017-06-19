fda43c3ec5b22447fad3a7e50d66885ae0757ebf
Julius Räihä <julius.raiha@phz.fi>
2017-06-19 13:58:48 +0300

    Reduced large breakpoint 1438 -> 1299 like in the design

--- themes/media-templates.js
+++ themes/media-templates.js
@@ -2,14 +2,14 @@
 import { css } from 'styled-components';
 
 const sizes = {
-  large: 1438,
+  large: 1299,
   medium: 999,
   small: 869,
 };
 
 // iterate through the sizes and create a media template
 export default Object.keys(sizes).reduce((accumulator, label) => {
-  // use em in breakpoints to work properly cross-browser and support users
+  // use rem in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
   const emSize = sizes[label] / 16;
   const returnObject = Object.assign({}, accumulator);
