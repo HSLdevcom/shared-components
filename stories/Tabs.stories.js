203da9359fcb088fb6656358a664fbab01986133
Julius Räihä <julius.raiha@phz.fi>
2017-06-27 16:02:01 +0300

    Tabs: improved stories with knobs

--- stories/Tabs.stories.js
+++ stories/Tabs.stories.js
@@ -4,7 +4,7 @@ import { setAddon, storiesOf } from '@kadira/storybook';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import JSXAddon from 'storybook-addon-jsx';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { withKnobs } from '@kadira/storybook-addon-knobs';
+import { withKnobs, number } from '@kadira/storybook-addon-knobs';
 
 import { Tabs, Tab, Div } from '../src';
 
@@ -16,28 +16,46 @@ stories.addDecorator(withKnobs);
 const StyledDiv = Div.extend`
     margin: 3em;
     width: 700px;
-    height: 300xp;
 `;
 
-stories.addWithJSX('default', () => (
-  <StyledDiv>
-    <Tabs index={1}>
-      <Tab header="Inactive">Content of inactive tab</Tab>
-      <Tab header="Active">Content of active tab</Tab>
-      <Tab header="Disabled" disabled>Content of disabled tab</Tab>
-    </Tabs>
-  </StyledDiv>)
-);
 
-stories.addWithJSX('rounded', () => (
-  <StyledDiv>
-    <Tabs index={1} rounded>
-      <Tab header="Inactive">Content of inactive tab</Tab>
-      <Tab header="Active">Content of active tab</Tab>
-      <Tab header="Disabled" disabled>Content of disabled tab</Tab>
-    </Tabs>
-  </StyledDiv>)
-);
+stories.addWithJSX('default', () => {
+  const options = {
+    range: true,
+    min: 0,
+    max: 2,
+    step: 1,
+  };
+  const index = number('Index', 1, options);
+  return (
+    <StyledDiv>
+      <Tabs index={index}>
+        <Tab header="First">Content of the first tab</Tab>
+        <Tab header="Second">Content of the second tab</Tab>
+        <Tab header="Third">Content of the third tab</Tab>
+        <Tab header="Disabled" disabled>Content of disabled tab</Tab>
+      </Tabs>
+    </StyledDiv>);
+});
+
+stories.addWithJSX('rounded', () => {
+  const options = {
+    range: true,
+    min: 0,
+    max: 2,
+    step: 1,
+  };
+  const index = number('Index', 1, options);
+  return (
+    <StyledDiv>
+      <Tabs index={index} rounded>
+        <Tab header="First">Content of the first tab</Tab>
+        <Tab header="Second">Content of the second tab</Tab>
+        <Tab header="Third">Content of the third tab</Tab>
+        <Tab header="Disabled" disabled>Content of disabled tab</Tab>
+      </Tabs>
+    </StyledDiv>);
+});
 
 stories.addWithJSX('one item', () => (
   <StyledDiv>
