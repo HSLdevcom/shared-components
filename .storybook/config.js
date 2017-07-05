e02a2f68c98468b9df5df81ff3dea908344bf281
Julius Räihä <julius.raiha@phz.fi>
2017-07-05 15:53:06 +0300

    stories moved to stories_web

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import Theme from './../themes/themes.hsl';
 
-const req = require.context('../stories', true, /\.stories\.js$/);
+const req = require.context('../stories_web', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
