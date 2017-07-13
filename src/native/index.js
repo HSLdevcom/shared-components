3fabecd424f0151fc2b4d47754dea162cc3def4b
Julius Räihä <julius.raiha@phz.fi>
2017-07-13 13:57:50 +0300

    Bumped version & changed Text from default export to named

--- src/native/index.js
+++ src/native/index.js
@@ -1,8 +1,7 @@
 import Text, { H1, H2, H3, H4, H5, P, Quote, Ingress, Caption, InfoText, ErrorText, ListText } from '../Typography';
 
-export default Text;
-
 export {
+  Text,
   H1,
   H2,
   H3,
