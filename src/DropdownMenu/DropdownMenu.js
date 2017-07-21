c03ea6cf9dde909346e400ec9a822a0453757c09
Julius Räihä <julius.raiha@phz.fi>
2017-07-21 10:22:35 +0300

    DropdownMenu: used native elements in dropdownmenu

--- src/DropdownMenu/DropdownMenu.js
+++ src/DropdownMenu/DropdownMenu.js
@@ -2,34 +2,62 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { ButtonNoStyle } from '../Button/Button';
 import Div from '../Div';
-import Span from '../Span';
 import A from '../Anchor';
 import DropdownContainerDesktop from './DropdownContainerDesktop';
 import DropdownContainerMobile from './DropdownContainerMobile';
 import { NavItem } from '../Nav';
 import { More, ArrowDown } from '../Icons';
+import Text from '../Typography';
+
+import View from '../View';
+import { size } from '../utils';
 
 const Icon = <More height="2.5rem" width="2.5rem" />;
-const Arrow = <ArrowDown height="0.5rem" width="0.75rem" />;
+
+const Arrow = styled(({ open, ...rest }) => (<ArrowDown height="0.5rem" width="0.75rem" {...rest} />))`
+  padding-left: ${size(8)};
+  height: ${size(8)} !important;
+  width: ${size(12)} !important;
+  #arrow {
+    transition: transform .25s linear;
+    transform-origin: center;
+    transform: rotate(0deg);
+  }
+  ${props => props.open && `
+    #arrow {
+      transform: rotate(-180deg);
+    }
+  `}
+`;
+
+const StyledView = View.extend`
+  flex-direction: row;
+`;
+
+const ButtonTitle = ({ children, open, ...rest }) => (
+  <StyledView {...rest}><Text>{children}</Text><Arrow open={open} /></StyledView>
+);
+
+ButtonTitle.propTypes = {
+  children: PropTypes.string,
+  open: PropTypes.bool
+};
+
+const ButtonNoStyle = styled.button`
+  padding: 0;
+  margin: 0;
+  border: 0;
+  outline: 0;
+  line-height: 0;
+  width: auto;
+  height: auto;
+  background: inherit;
+  color: inherit;
+  font: inherit;
+`;
 
 const StyledDiv = Div.extend`
-  ${NavItem} ${ButtonNoStyle} ${Span} svg {
-    padding-left: 0.5rem;
-    height: 0.5rem;
-    width: 0.75rem;
-    #arrow {
-      transition: transform .25s linear;
-      transform-origin: center;
-      transform: rotate(0deg);
-    }
-    ${props => props.open && `
-      #arrow {
-        transform: rotate(-180deg);
-      }
-    `}
-  }
   ${DropdownContainerMobile} {
     display: none;
   }
@@ -96,7 +124,7 @@ class DropdownMenu extends React.Component {
           <NavItem
             link={<A />}
             icon={Icon}
-            text={<Span>{this.props.text}{Arrow}</Span>}
+            text={<ButtonTitle open={this.state.open}>{this.props.text}</ButtonTitle>}
             textPosition={this.props.textPosition}
             active={this.props.active}
             small={this.props.small}
