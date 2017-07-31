6be03043209f702d3dd66c13829321c4fd520b63
Julius Räihä <julius.raiha@phz.fi>
2017-07-31 13:43:49 +0300

    Storybook native: ran eject

--- storybooks/native/App.js
+++ storybooks/native/App.js
@@ -1,47 +1,26 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Font } from 'expo';
 import { ThemeProvider } from 'styled-components';
 
 import Theme from './themes/themes.hsl';
 import StorybookUI from './storybook';
 
-const font = require('./static/Digitalt.ttf');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { fontLoaded: false };
-  }
+const App = () => (
+  <ThemeProvider theme={Theme}>
+    <View style={styles.container}>
+      <StorybookUI />
+    </View>
+  </ThemeProvider>
+);
 
-  async componentDidMount() {
-    this.loadFont();
-  }
-
-  async loadFont() {
-    await Font.loadAsync({
-      'Gotham Rounded SSm A, Gotham Rounded SSm B': font,
-      'Gotham XNarrow SSm A, Gotham XNarrow SSm B': font,
-    });
-    this.setState({ fontLoaded: true });
-  }
-
-  render() {
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
-    return (
-      <ThemeProvider theme={Theme}>
-        <View style={styles.container}>
-          { this.state.fontLoaded && <StorybookUI />}
-        </View>
-      </ThemeProvider>
-    );
-  }
-}
+export default App;
