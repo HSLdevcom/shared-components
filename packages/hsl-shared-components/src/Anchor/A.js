c7082432bb240e55910f5c1c85ffb918b44269a4
Julius Räihä <julius.raiha@phz.fi>
2017-07-31 09:54:27 +0300

    Anchor: removed default a-tag styles

--- packages/hsl-shared-components/src/Anchor/A.js
+++ packages/hsl-shared-components/src/Anchor/A.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
-const A = styled.a``;
+const A = styled.a`
+  color: inherit;
+  text-decoration: none;
+  &:link, &:visited, &:link:active, &:visited:active {
+    color: inherit;
+  }
+`;
 
 export default A;
 
