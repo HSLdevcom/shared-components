270fb8f7541a0ecd1110839277781c1b4461ae4f
Hannes Junnila <hannes.junnila@gmail.com>
2017-08-24 11:12:05 +0300

    Remove nesting in PageTitileBar

--- packages/hsl-shared-components/src/PageTitleBar/PageTitleBar.js
+++ packages/hsl-shared-components/src/PageTitleBar/PageTitleBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Div from '../Div';
 
 const StyledDiv = Div.extend`
@@ -8,62 +9,63 @@ const StyledDiv = Div.extend`
   width: 100vw;
   left: calc(-50vw + 50%);
   margin-bottom: 1em;
-  .content-container {
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    max-width: 1100px;
-    margin: auto;
-    h1 {
-      font-size: 1.2em;
-      letter-spacing: -0.8px;
-      color: ${props => props.theme.primary};
-      text-align: center;
-      margin: 1em auto;
-      font-weight: normal;
-    }
-    .back-button {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: absolute;
-      top: 0;
-      left: 0;
-      font-size: 1.2em;
-      width: 3em;
-      height: 100%;
-      text-align: center;
-      border-width: 0;
-      border: none;
-      background-color: #fff;
-      border-right: 1px solid #ddd;
-      padding: 0;
-      &:hover {
-        cursor: pointer;
-      }
-      img {
-        height: 1em;
-        margin: auto;
-      }
-      svg {
-        margin: auto;
-      }
-    }
+`;
+
+const ContentContainer = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  max-width: 1100px;
+  margin: auto;
+  h1 {
+    font-size: 1.2em;
+    letter-spacing: -0.8px;
+    color: ${props => props.theme.primary};
+    text-align: center;
+    margin: 1em auto;
+    font-weight: normal;
   }
 `;
 
+const BackButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-size: 1.2em;
+  width: 3em;
+  height: 100%;
+  text-align: center;
+  border-width: 0;
+  border: none;
+  background-color: #fff;
+  border-right: 1px solid #ddd;
+  padding: 0;
+  &:hover {
+    cursor: pointer;
+  }
+  img {
+    height: 1em;
+    margin: auto;
+  }
+  svg {
+    margin: auto;
+  }
+`;
 
 const PageTitleBar = ({ children, backButton, onBackClick, className }) =>
   <StyledDiv className={className}>
-    <div className="content-container">
+    <ContentContainer>
       {backButton &&
-        <button className="back-button" onClick={onBackClick}>
+        <BackButton onClick={onBackClick}>
           {backButton}
-        </button>
+        </BackButton>
       }
       {children}
-    </div>
+    </ContentContainer>
   </StyledDiv>;
 
 PageTitleBar.propTypes = {
