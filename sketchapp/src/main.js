4b5ab52ad359d762ae03d7ec5950905195bb62fe
Mika Lindell <mika.lindell82@gmail.com>
2017-09-13 11:34:23 +0300

    Added all sections in UI kit

--- sketchapp/src/main.js
+++ sketchapp/src/main.js
@@ -16,11 +16,16 @@ const Document = () =>
       >
         <Section separator>
           <Title>
+            Colors
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
             Typography
           </Title>
           <Typography />
         </Section>
-        <Section>
+        <Section separator>
           <Title>
             Buttons
           </Title>
@@ -28,6 +33,134 @@ const Document = () =>
             Primary buttons - example CTA
           </SubTitle>
           <PrimaryButtons />
+        </Section>
+        <Section separator>
+          <Title>
+            Links
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Inputs
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Checkboxes, Radiobuttons, Toggle buttons
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Sliders
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Dropdown
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Tabs
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Progress Bars
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Notifications
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Header
+          </Title>
+          <Title>
+            Mobile Header / menu
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Icons
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Cards
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Modal
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Images
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Tables
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Enter origin / locale, Destination search
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Nearby
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Accordion
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Q&A Accordion
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Divider
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Spinner
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Favourite links
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Breadcrumbs
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Tags
+          </Title>
+        </Section>
+        <Section separator>
+          <Title>
+            Leaflet popup
+          </Title>
+        </Section>
+        <Section>
+          <Title>
+            Footer
+          </Title>
         </Section>
       </Artboard>
     </ThemeProvider>
