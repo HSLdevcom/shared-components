2687e91518ad0efc7c298d2de3aff2ab577981af
Julius Räihä <julius.raiha@phz.fi>
2017-09-14 11:50:16 +0300

    Changed Moved colors from media-templates to own file

---
+++ storybooks/native/themes/colors.js
@@ -0,0 +1,23 @@
+const colors = {
+  primary: {
+    hslBlue: '#007ac9',
+    hslBlueDark: '#0062a1',
+    hslGreen: '#4ea700',
+    hslGrey: '#888888',
+    hslGreyDark: '#333333',
+  },
+  secondary: {
+    hslOrange: '#ff6319',
+    hslMagenta: '#dc0451',
+    hslPurple: '#8c4799',
+    hslYellow: '#fed100',
+    hslPink: '#f092cd',
+  },
+  background: {
+    hslWhite: '#ffffff',
+    hslBlueLight: '#eff9ff',
+    hslGreyLight: '#eef1f3',
+  }
+};
+
+export default colors;
