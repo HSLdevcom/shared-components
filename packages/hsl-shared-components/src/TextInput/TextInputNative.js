322cf912ac4fa6bdeff595cc22d6cdbdab820060
Julius Räihä <julius.raiha@phz.fi>
2017-09-18 10:30:42 +0300

    TextInput native: removed underline

--- packages/hsl-shared-components/src/TextInput/TextInputNative.js
+++ packages/hsl-shared-components/src/TextInput/TextInputNative.js
@@ -6,7 +6,6 @@ const TextInput = styled.TextInput`
   font-size: ${size(18)};
   ${props => props.theme.fontFamily && `font-family: ${props.theme.fontFamily};`}
   color: ${props => props.theme.secondary};
-
   padding: ${size(14)} ${size(18)};
   border-radius: ${size(4)};
   border-style: solid;
@@ -23,37 +22,8 @@ const TextInput = styled.TextInput`
   `}
 `;
 
-/*
-const Input = styled.input `
-  font-size: ${size(18)};
-  ${props => props.theme.fontFamily && `font-family: ${props.theme.fontFamily};`}
-  color: ${props => props.theme.secondary};
+TextInput.defaultProps = {
+  underlineColorAndroid: 'transparent'
+};
 
-  padding: ${size(14)} ${size(18)};
-  border-radius: ${size(4)};
-  border: solid 1px ${props => lighten(0.185, props.theme.secondary)};
-  outline: 0;
-  transition: border-color 0.5s, color 0.5s;
-  &:focus {
-    color: #000000;
-    border-color: ${props => props.theme.primary};
-  }
-  ${props => props.focus && `
-    color: #000000;
-    border-color: ${props.theme.primary};
-  `}
-  ${props => props.error && `
-    color: ${props.theme.error};
-    border-color: ${props.theme.error};
-    background-color: ${props.theme.errorBackground};
-  `}
-  &:focus {
-    ${props => props.error && `
-      color: ${props.theme.error};
-      border-color: ${props.theme.error};
-      background-color: ${props.theme.errorBackground};
-    `}
-  }
-`;
-*/
 export default TextInput;
