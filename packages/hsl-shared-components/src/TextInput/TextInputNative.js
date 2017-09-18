f41d4f07ad67c12dd8bddbea2b95f0ba904cf277
Julius Räihä <julius.raiha@phz.fi>
2017-09-18 13:34:17 +0300

    TextInputNative: made components stateful (to handle focus/blur & value change)

--- packages/hsl-shared-components/src/TextInput/TextInputNative.js
+++ packages/hsl-shared-components/src/TextInput/TextInputNative.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import { lighten } from 'polished';
+import _ from 'lodash';
 import { size } from '../utils';
 
-const TextInput = styled.TextInput`
+const Input = styled.TextInput`
   font-size: ${size(18)};
   ${props => props.theme.fontFamily && `font-family: ${props.theme.fontFamily};`}
   color: ${props => props.theme.secondary};
@@ -12,18 +15,81 @@ const TextInput = styled.TextInput`
   border-width: 1px;
   border-color: ${props => lighten(0.185, props.theme.secondary)};
   ${props => props.focus && `
-    color: ${props.theme.colors.hslGreyDark};
+    color: ${props.theme.colors.primary.hslGreyDark};
     border-color: ${props.theme.primary};
   `}
   ${props => props.error && `
-    color: ${props.theme.error};
-    border-color: ${props.theme.error};
-    background-color: ${props.theme.errorBackground};
+    color: ${props.theme.colors.misc.error};
+    border-color: ${props.theme.colors.misc.error};
+    background-color: ${props.theme.colors.misc.errorBackground};
   `}
 `;
 
-TextInput.defaultProps = {
+Input.defaultProps = {
   underlineColorAndroid: 'transparent'
 };
 
-export default TextInput;
+class TextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { focus: false, value: this.props.value || '' };
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onFocus() {
+    this.setState({
+      focus: true
+    });
+
+    if (this.props.onFocus) {
+      this.props.onFocus();
+    }
+  }
+
+  onBlur() {
+    this.setState({
+      focus: false
+    });
+
+    if (this.props.onBlur) {
+      this.props.onBlur();
+    }
+  }
+
+  onChange(value) {
+    this.setState({
+      value
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange();
+    }
+  }
+
+  render() {
+    return (<Input
+      {..._.omit(this.props, ['onChange', 'onBlur', 'onFocus'])}
+      value={this.state.value}
+      focus={this.state.focus}
+      onBlur={this.onBlur}
+      onFocus={this.onFocus}
+      onChangeText={this.onChange}
+    />);
+  }
+}
+
+TextInput.propTypes = {
+  onChange: PropTypes.func,
+  onBlur: PropTypes.func,
+  onFocus: PropTypes.func,
+  value: PropTypes.string,
+  error: PropTypes.bool // eslint-disable-line react/no-unused-prop-types
+};
+
+const StyledTextInput = styled(TextInput)``;
+
+StyledTextInput.displayName = 'TextInput';
+
+export default StyledTextInput;
