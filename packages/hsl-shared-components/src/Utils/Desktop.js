b182fa154d2b819dd3d029673d28bc23518a856c
Julius Räihä <julius.raiha@phz.fi>
2017-09-20 16:31:13 +0300

    Removed full lodash imports

--- packages/hsl-shared-components/src/Utils/Desktop.js
+++ packages/hsl-shared-components/src/Utils/Desktop.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import { Dimensions } from 'react-primitives';
 import { withTheme } from 'styled-components';
 import styled from 'styled-components/primitives';
-import _ from 'lodash';
+import omit from 'lodash/fp/omit';
+import debounce from 'lodash/debounce';
 
 import View from '../View';
 import { IS_NATIVE } from '../utils';
@@ -13,7 +14,7 @@ class Desktop extends React.Component {
   constructor(props) {
     super(props);
     this.state = { width: Dimensions.get('window').width };
-    this.onResize = _.debounce(this.onResize.bind(this), 200);
+    this.onResize = debounce(this.onResize.bind(this), 200);
   }
 
   componentDidMount() {
@@ -37,7 +38,7 @@ class Desktop extends React.Component {
 
   render() {
     return (!IS_NATIVE && this.state.width > this.props.theme.sizes.small) &&
-    <View {..._.omit(this.props, 'theme')} />;
+    <View {...omit(this.props, 'theme')} />;
   }
 }
 
