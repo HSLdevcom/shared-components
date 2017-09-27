8ebef6a707fbe0e71122c4c072eb707465eb581e
Mika Lindell <mika.lindell82@gmail.com>
2017-09-27 09:47:06 +0300

    Fixed unknown prop -warnings

--- packages/hsl-shared-components/src/Icons/ArrowUp.js
+++ packages/hsl-shared-components/src/Icons/ArrowUp.js
@@ -7,13 +7,23 @@ import { Svg,
 
 import PropTypes from 'prop-types';
 
-import { svgSize } from '../utils';
+import { IS_NATIVE, svgSize } from '../utils';
 
 export default function Icon({ fill, height, width, ...rest }) {
+  const flipNative = IS_NATIVE ? {
+    rotate: 180,
+    originX: 21.0,
+    originY: 13.0,
+  } : {};
+
+  const flipWeb = !IS_NATIVE ? {
+    transform: 'scale(1, -1), translate(0, -27.47)',
+  } : {};
+
   return (
     <Svg {...rest} {...svgSize(height, width)} viewBox="0 0 42.5 27.47" version="1.1" preserveAspectRatio="xMidYMid meet">
-      <G rotate={180} originX={8.5} originY={17.5}>
-        <Path transform="scale(1, -1), translate(0, -27.47)" fill={fill} d="M35.043,1.271 L21.214,15.140 L7.384,1.271 C5.687,-0.431 2.934,-0.431 1.234,1.271 L1.234,1.271 C-0.463,2.973 -0.463,5.733 1.234,7.436 L21.214,27.468 L41.191,7.436 C42.887,5.733 42.887,2.973 41.191,1.271 L41.191,1.271 C39.493,-0.431 36.743,-0.431 35.043,1.271 Z" id="arrow" />
+      <G {...flipNative}>
+        <Path {...flipWeb} fill={fill} d="M35.043,1.271 L21.214,15.140 L7.384,1.271 C5.687,-0.431 2.934,-0.431 1.234,1.271 L1.234,1.271 C-0.463,2.973 -0.463,5.733 1.234,7.436 L21.214,27.468 L41.191,7.436 C42.887,5.733 42.887,2.973 41.191,1.271 L41.191,1.271 C39.493,-0.431 36.743,-0.431 35.043,1.271 Z" id="arrow" />
       </G>
     </Svg>
   );
