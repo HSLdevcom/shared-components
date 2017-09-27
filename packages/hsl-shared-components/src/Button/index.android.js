9a7313ef33ca002c6519c7bd3ae88fbedb4f8265
Mika Lindell <mika.lindell82@gmail.com>
2017-09-27 13:58:46 +0300

    Removed unused code

--- packages/hsl-shared-components/src/Button/index.android.js
+++ packages/hsl-shared-components/src/Button/index.android.js
@@ -1,12 +1,10 @@
 import Button from './ButtonNative';
 import RoundButton from './RoundButtonNative';
 import ButtonWithText from './ButtonWithText';
-import FaqButton from './FaqButton';
 
 export {
   RoundButton,
   ButtonWithText,
-  FaqButton,
 };
 
 export default Button;
--- packages/hsl-shared-components/src/Button/index.js
+++ packages/hsl-shared-components/src/Button/index.js
@@ -1,12 +1,10 @@
 import Button from './Button';
 import RoundButton from './RoundButton';
 import ButtonWithText from './ButtonWithText';
-import FaqButton from './FaqButton';
 
 export {
   RoundButton,
   ButtonWithText,
-  FaqButton,
 };
 
 export default Button;
