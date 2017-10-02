831786682edee8649d623cb967dd8bc55957c776
Mika Lindell <mika.lindell82@gmail.com>
2017-10-02 13:27:25 +0300

    Native version of actionBar

--- storybooks/native/stories/ActionBar.stories.js
+++ storybooks/native/stories/ActionBar.stories.js
@@ -3,9 +3,32 @@ import { storiesOf } from '@storybook/react-native';
 import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { ThemeProvider } from 'styled-components';
-import { ActionBarItem, Icons, ResponsiveProvider, Theme } from 'hsl-shared-components';
+import { ActionBar, ActionBarItem, Icons, ResponsiveProvider, Theme } from 'hsl-shared-components';
 
 import CenterView from './CenterView';
+
+const items = [
+  {
+    title: 'Lippuja ja hintoja',
+    icon: <Icons.Tickets />,
+  },
+  {
+    title: 'Reittejä, aikatauluja tai matkustamista',
+    icon: <Icons.JourneyPlanner />,
+  },
+  {
+    title: 'Löytötavaroita',
+    icon: <Icons.Info />,
+  },
+  {
+    title: 'HSL:n sähköisiä palveluita',
+    icon: <Icons.MobileTicket />,
+  },
+  {
+    title: 'Palveluita yrityksille ja oppilaitoksille',
+    icon: <Icons.CustomerService />,
+  }
+];
 
 storiesOf('ActionBar', module)
   .addDecorator(withKnobs)
@@ -16,6 +39,15 @@ storiesOf('ActionBar', module)
       </ResponsiveProvider>
     </ThemeProvider>))
   .add('Default', () => {
+    const inverted = boolean('Inverted', false);
+    return (
+      <ActionBar
+        items={items}
+        inverted={inverted}
+      />
+    );
+  })
+  .add('ActionBarItem', () => {
     const title = text('Title', 'Ruskeasuon varikko');
     const iconKnob = boolean('Icon', true);
     const icon = iconKnob ? <Icons.JourneyPlanner /> : null;
