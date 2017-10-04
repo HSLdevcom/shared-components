07e8c77eb8d07eb16b3883b8dc991e5741618657
Julius Räihä <julius.raiha@phz.fi>
2017-10-04 13:29:30 +0300

    Nav desktop scroll animation

--- packages/hsl-shared-components/src/Nav/Nav.js
+++ packages/hsl-shared-components/src/Nav/Nav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import omit from 'lodash/fp/omit';
+import { Animated } from 'react-primitives';
 
 import NavDesktop from './NavDesktop';
 import NavMobile from './NavMobile';
@@ -14,15 +15,17 @@ const Header = View.extend`
   align-items: stretch;
 `;
 
-const ScrollWrap = styled.div`
+const Div = Animated.createAnimatedComponent('div');
+
+const ScrollWrap = styled(props => (
+  <Div {...props} />
+))`
   position: fixed;
   width: 100%;
   top: 0;
+  overflow: hidden;
 `;
 
-const ScrollNav = props => (
-  <ScrollWrap><NavDesktop {...props} scroll /></ScrollWrap>
-);
 
 class Nav extends React.Component {
   constructor(props) {
@@ -30,10 +33,11 @@ class Nav extends React.Component {
     this.state = {
       desktopScrollNav: false,
       mobileNavVisible: true,
-      navHeight: 0
+      anim: new Animated.Value(0)
     };
     this.lastScrollValue = 0;
     this.ticking = false;
+    this.navHeight = 0;
     this.onScroll = this.onScroll.bind(this);
     this.onLayout = this.onLayout.bind(this);
   }
@@ -64,9 +68,7 @@ class Nav extends React.Component {
   }
 
   onLayout(e) {
-    this.setState({
-      navHeight: e.nativeEvent.layout.height
-    });
+    this.navHeight = e.nativeEvent.layout.height;
   }
 
   onScrollDesktop(scrollY) {
@@ -75,7 +77,7 @@ class Nav extends React.Component {
 
     // When nav is not visible, dont show nav unless we have scrolled 3 x past nav height
     // When nav is visible, hide nav when we reach the bottom of nav
-    const boundary = this.state.desktopScrollNav ? this.state.navHeight : 3 * this.state.navHeight;
+    const boundary = this.state.desktopScrollNav ? this.navHeight : 3 * this.navHeight;
 
     const scrolledPastBoundary = boundary < scrollY;
 
@@ -83,12 +85,18 @@ class Nav extends React.Component {
 
     // Call setState only if state will change
     // TODO: Remove the if after upgrading to react 16 where canceling setState is possible
-    if ((scrolledPastBoundary && scrollingUp) !== this.state.desktopScrollNav) {
-      this.setState(prevState => (
-        (scrolledPastBoundary && scrollingUp) !== prevState.desktopScrollNav
-        ? { desktopScrollNav: scrolledPastBoundary && scrollingUp }
-        : undefined
-      ));
+    if ((scrolledPastBoundary && scrollingUp) && !this.state.desktopScrollNav) {
+      this.setState({ desktopScrollNav: true }, () => {
+        Animated.timing(this.state.anim, { toValue: 64, duration: 150 }).start();
+      });
+      return;
+    }
+    if (!(scrolledPastBoundary && scrollingUp) && this.state.desktopScrollNav && !this.closing) {
+      this.closing = true;
+      Animated.timing(this.state.anim, { toValue: 0, duration: 150 }).start(() => {
+        this.setState({ desktopScrollNav: false });
+        this.closing = false;
+      });
     }
   }
 
@@ -129,18 +137,21 @@ class Nav extends React.Component {
               {this.props.children}
             </NavDesktop>}
         />
-        {
-          this.state.desktopScrollNav && <Responsive
-            small={null}
-            medium={
-              <ScrollNav
-                logo={this.props.logo}
-                menu={<MenuSmall {...this.props.menu.props} />}
-              >
-                {this.props.children}
-              </ScrollNav>}
-          />
-        }
+        <ScrollWrap style={{ maxHeight: this.state.anim }}>
+          {
+            this.state.desktopScrollNav && <Responsive
+              small={null}
+              medium={
+                <NavDesktop
+                  logo={this.props.logo}
+                  menu={<MenuSmall {...this.props.menu.props} />}
+                  scroll
+                >
+                  {this.props.children}
+                </NavDesktop>}
+            />
+          }
+        </ScrollWrap>
       </Header>
     );
   }
